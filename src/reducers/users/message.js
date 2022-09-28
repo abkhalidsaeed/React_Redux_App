ecc8627c77946feaff6fc6d5b5d71d2390d7e27c
Abdullah Khalid <abdullahkhalidz114@gmail.com>
2022-09-28 09:36:05 +0500

    redux store add

--- src/reducers/users/message.js
+++ src/reducers/users/message.js
@@ -1,5 +1,18 @@
-import { SET_MESSAGE, CLEAR_MESSAGE } from "../../actions/users/types"
+import { SET_MESSAGE, CLEAR_MESSAGE } from "../../actions/users/types";
 
 const intialState = {};
 
-export default function (state = intialState)
+export default function (state = intialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SET_MESSAGE:
+      return { message: payload };
+
+    case CLEAR_MESSAGE:
+      return { message: "" };
+
+    default:
+      return state;
+  }
+}
